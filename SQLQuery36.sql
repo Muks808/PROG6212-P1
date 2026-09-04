080cb619a066b0f02577b7b96e65d1ea2e2f42bc
Muks808 <princemashau26@gmail.com>
2026-09-04 18:57:09 +0200

    Add files via upload

---
+++ SQLQuery36.sql
@@ -0,0 +1,15 @@
+SELECT * FROM dbo.Users;
+
+SELECT * FROM dbo.Events;
+
+SELECT * FROM dbo.Categories;
+
+SELECT * FROM dbo.Enrolments;
+
+SELECT * FROM dbo.Results;
+
+SELECT * FROM dbo.EventImages;
+
+SELECT * FROM dbo.ProfileImages;
+
+GO
